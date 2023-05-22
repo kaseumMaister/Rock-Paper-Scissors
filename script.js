c1adab05eb440a02ed6ac38e6fedf278fa168cdb
RafaelSouzaUbuntu <officiumrflss@gmail.com>
2023-05-22 16:40:31 -0300

    First UI for this RPS game

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
-function play5Rounds () {
-    for (i = 1; i <= 5; i++) {
-        let round = playRound();
-        console.log(round);
-    }
-    
-    function playRound () {
-        let pcPick = getComputerAnswer();
-        let playerPick = prompt(`Choose among: 'Rock', 'Paper', and 'Scissors' `);
-        let result = '';
 
-        if (pcPick === playerPick) {
-            result = "That's a tie, Fellas!!"
-        } else if (pcPick === "Rock" && playerPick === "Scissors" ||
-        pcPick === "Paper" && playerPick === "Rock"  ||
-        pcPick === "Scissors" && playerPick === "Paper") {
-            result = "PC Wins"
-        } else {
-            result = "Player wins"
+    let rounds = 0;
+        let playerScore = 0;
+        let computerScore = 0;
+
+        function playRound(playerChoice) {
+            if (rounds >= 5) {
+                determineWinner();
+                return;
+            }
+
+            let pcPick = getComputerAnswer();
+            let playerPick = playerChoice;
+            let scoreBoard = document.querySelector("#scoreboard");
+            let result = '';
+
+            if (pcPick === playerPick) {
+                result = "That's a tie, Fellas!!";
+            } else if (
+                (pcPick === "Rock" && playerPick === "Scissors") ||
+                (pcPick === "Paper" && playerPick === "Rock") ||
+                (pcPick === "Scissors" && playerPick === "Paper")
+            ) {
+                result = "Computer Wins";
+                computerScore++;
+            } else {
+                result = "Player wins";
+                playerScore++;
+            }
+            
+            rounds++;
+            scoreBoard.innerText += result + "\n";
+
+            if (rounds >= 5) {
+                determineWinner();
+            }
         }
-         return `PC:${pcPick} x ${playerPick} :Player Round score is:  ${result}`;
 
-
-        function getComputerAnswer () {
-            let number = Math.floor(Math.random() *10) + 1;
+        function getComputerAnswer() {
+            let number = Math.floor(Math.random() * 10) + 1;
             let pcPick = '';
 
             if (number % 3 === 0) {
@@ -34,7 +49,17 @@ function play5Rounds () {
             }
             return pcPick;
         }
-    }
-}
 
-play5Rounds();
+        function determineWinner() {
+            let scoreBoard = document.querySelector("#scoreboard");
+            scoreBoard.innerText += "\n------ Game Over ------\n";
+            scoreBoard.innerText += "Player Score: " + playerScore + "\n";
+            scoreBoard.innerText += "Computer Score: " + computerScore + "\n";
+            if (playerScore > computerScore) {
+                scoreBoard.innerText += "Player Wins!";
+            } else if (playerScore < computerScore) {
+                scoreBoard.innerText += "Computer Wins!";
+            } else {
+                scoreBoard.innerText += "It's a tie!";
+            }
+        }
